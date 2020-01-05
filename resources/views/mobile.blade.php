4bc3a7dab6fc28856e7c44ad434082a503ab3eb4
李均洽 <9623595@qq.com>
2020-01-05 13:51:06 +0800

    优化

--- resources/views/mobile.blade.php
+++ resources/views/mobile.blade.php
@@ -250,9 +250,8 @@
                 //弹出面板
                 layim.panel({
                     title: title //标题
-                    ,tpl: '<div style="padding: 10px;">自定义模版，chat_log</div>' //模版，基于laytpl语法
+                    ,tpl: res //模版，基于laytpl语法
                     ,data: { //数据
-                        rows: res.data //假设rows为群组成员
                     }
                 });
             });
